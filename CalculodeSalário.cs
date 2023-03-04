29caedc334d8fbe781b8894697df55aa624f89b3
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-04 16:00:28 -0300

    Add files via upload
    Programa para ler o número de um funcionário, a quantidade de horas trabalhadas e o valor que recebe por hora. O programa calcula e mostra o salário deste funcionário.

---
+++ CalculodeSalário.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+       string numeroFunc;
+       float ht;
+       float vh;
+       float salario;
+       int diasemana;
+       int diames;
+       Console.WriteLine("Qual o seu número de funcionário?");
+       numeroFunc=Console.ReadLine();
+       Console.WriteLine("Digite a quantidade de horas trabalhadas por dia");
+       ht=float.Parse(Console.ReadLine());
+       Console.WriteLine("Qual o valor recebido por hora?");
+       vh=float.Parse(Console.ReadLine());
+       Console.WriteLine("Quantos dias por semana você trabalha?");
+       diasemana=int.Parse(Console.ReadLine());
+       diames= diasemana * 4;
+       salario=vh*ht*diames;
+       Console.WriteLine("O salário do funcionário é de " + salario);
+    }
+}
+
+    
+

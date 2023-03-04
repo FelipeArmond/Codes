a0db88a6b7cbb42764d6446ebcb44848acaec7e6
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-04 16:14:56 -0300

    Área de trapézio
    Programa que calcula e mostra a área de um trapézio.

---
+++ AreadeTrapezio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+               float basemaior;
+        float basemenor;
+        float altura;
+        float A;
+        Console.WriteLine("Qual o valor da base maior?");
+        while (!float.TryParse(Console.ReadLine(), out basemaior))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um valor numérico");
+        }
+        Console.WriteLine("Qual o valor da base menor?");
+        while(!float.TryParse(Console.ReadLine(), out basemenor))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um valor numérico");
+        }
+        Console.WriteLine("Qual a altura?");
+        while(!float.TryParse(Console.ReadLine(), out altura)){
+            Console.WriteLine("Entrada inválida. Por favor, insira um valor numérico");
+        }
+        A = ((basemaior + basemenor) * altura)/2;
+        Console.WriteLine("A área do trapézio é de " + A);
+    }
+}
+
+
+    
+

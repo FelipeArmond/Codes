ca223ec7cc8467148e98af8c34940ab86740198c
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-10 11:44:05 -0300

    Update calculoMaioridade.cs

--- calculoMaioridade.cs
+++ calculoMaioridade.cs
@@ -29,6 +29,6 @@ namespace MyProgram
     }
 }
 
-// pprograma  para  determinar  se  uma  pessoa  é  maior  ou  menor  de  idade. A maioridade civil é atingida quando a pessoa completa 16 anos. A maioridade penal é atingida quando a pessoa complete  18  anos. A  pessoa irá fornecer o ano atua  e o seu  ano  de  nascimento. O programa deverá imprimir se a pessoa já tem maioridade civil ou penal. 
+
 // Felipe de Pinho Dias Armond
 // 09/03/2023

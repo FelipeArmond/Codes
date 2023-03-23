e080f9e22b30e86d6a321a075a852c0326eec310
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-23 15:46:13 -0300

    Add files via upload
    Programa que lê três números reais representando os lados de um triângulo e informa se seu triângulo é
    Equilátero, Isósceles ou Escaleno.

---
+++ ladosTriangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace listassequenciais {
+    class program {
+        static void Main(string[] args) 
+        {
+            int ladotriangulo1;
+            int ladotriangulo2;
+            int ladotriangulo3;
+
+            Console.WriteLine("Digite o valor do primeiro lado do triângulo : ");
+            ladotriangulo1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do segundo lado do triângulo : ");
+            ladotriangulo2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do terceiro lado do triângulo : ");
+            ladotriangulo3 = int.Parse(Console.ReadLine());
+
+            if (ladotriangulo1 == ladotriangulo2 && ladotriangulo2 == ladotriangulo3) 
+            {
+                Console.WriteLine("Esse é um triângulo equilátero");
+            }
+
+            else if (ladotriangulo1 != ladotriangulo2 && ladotriangulo2!= ladotriangulo3 && ladotriangulo3 != ladotriangulo1) 
+            {
+                Console.WriteLine("Esse é um triângulo escaleno");
+            }
+
+            else 
+            {
+                Console.WriteLine("Esse é um triângulo isoceles");
+            }
+
+        }
+    }
+}

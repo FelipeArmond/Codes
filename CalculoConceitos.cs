f01e451735dd0c64dee8e600a7525a75ae1b19ea
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-23 15:43:31 -0300

    Add files via upload
    A nota final de um estudante é calculada a partir de três notas atribuídas, respectivamente, a um trabalho de
    laboratório, a uma avaliação semestral e a um exame final. A média das três notas mencionadas obedece aos pesos a
    seguir:
    Nota Peso
    Trabalho de laboratório 2
    Avaliação semestral 3
    Exame final 5
     Programa que receba 3 notas, calcula e mostra a média ponderada e o conceito que segue a tabela:
    Média ponderada Conceito
    >=8 e <=10          A
    >=7 e < 8              B
    >=6 e < 7              C
    >=5 e < 6              D
    >=0 e < 5              E

---
+++ CalculoConceitos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace listassequenciais {
+    class program {
+        static void Main(string[] args) 
+        {
+            int trabalholab;
+            int avalsemestral;
+            int examefinal;
+            int notatrab;
+            int notaaval;
+            int notaexame;
+            float mediaponderada;
+
+            Console.WriteLine("Qual a nota obtida no trabalho de laboratório?");
+            trabalholab = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a nota obtida na avaliação semestral?");
+            avalsemestral = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a nota obtida no exame final?");
+            examefinal = int.Parse(Console.ReadLine());
+
+            notatrab = trabalholab * 2;
+            notaaval = avalsemestral * 3;
+            notaexame = examefinal * 5;
+            mediaponderada = (notatrab + notaaval + notaexame )/ (2 + 3 + 5);
+
+            if (mediaponderada >= 8 && mediaponderada <= 10) 
+            {
+                Console.WriteLine("Conceito A");
+            }
+            else if (mediaponderada >= 7 && mediaponderada <8) 
+            {
+                Console.WriteLine("Conceito B");
+            }
+            if (mediaponderada >= 6 && mediaponderada <= 7) 
+            {
+                Console.WriteLine("Conceito C");
+            }
+            else if (mediaponderada >= 5 && mediaponderada < 6) 
+            {
+                Console.WriteLine("Conceito D");
+            }
+            if (mediaponderada >= 0 && mediaponderada < 5) 
+            {
+                Console.WriteLine("Conceito E");
+            }
+        }
+    }
+}

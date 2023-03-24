c1a6fac51b1e5d980511b2c518c17474b4d35469
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-24 14:00:14 -0300

    Add files via upload
    Programa que recebe o código de origem de um produto e mostre seu nome. Caso o usuário digite outro código, deve aparecer na tela “Código Inválido”.

---
+++ programCodigoproduto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lista_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+
+            Console.WriteLine("Qual o código do produto?");
+            codigo = int.Parse(Console.ReadLine());
+
+            switch (codigo)
+            {
+                case 11:
+                    Console.WriteLine("Arroz");
+                break;
+                case 12:
+                    Console.WriteLine("Feijão");
+                break;
+                case 13:
+                     Console.WriteLine("Batata");
+                break;
+                case 14:
+                    Console.WriteLine("Carne");
+                break;
+                default:
+                    Console.WriteLine("Código invalido");
+                break;
+            }
+            
+        }
+    }
+}

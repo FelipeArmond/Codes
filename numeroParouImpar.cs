9f8a20c9be4f93c94db35bafe8804a72c43d5d0f
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-26 14:31:07 -0300

    Add files via upload
    Programa que recebe um número inteiro e verifique se é par ou ímpar.

---
+++ numeroParouImpar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Myprogram 
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+           int numero;
+           
+           Console.WriteLine("Digite um número inteiro:" );
+           numero = int.Parse(Console.ReadLine());
+
+           if (numero % 2 == 0)
+           {
+            Console.WriteLine("Esse é um número par");
+           }
+           else 
+           {
+            Console.WriteLine("Esse é um número impar");
+           }
+        }
+    }
+}

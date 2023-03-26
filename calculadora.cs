e955f86275d0342e8be35ced249f05be0b2e422e
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-03-26 13:57:41 -0300

    Add files via upload
    Programa que simula uma calculadora com as quatro operações básicas.

---
+++ calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Myprogram 
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            int numero1;
+            int numero2;
+            String sinal;
+            int soma;
+            int multiplicacao;
+            int subtracao;
+            double divisao;
+
+            Console.WriteLine("Digite a operação numérica:");
+            (numero1) = int.Parse(Console.ReadLine());
+            sinal = Console.ReadLine();
+            (numero2) = int.Parse(Console.ReadLine());
+           
+
+            soma = numero1 + numero2;
+            subtracao = numero1 - numero2;
+            multiplicacao = numero1 * numero2;
+            divisao = numero1 / numero2;
+            
+            switch (sinal)
+            {
+                case "+" :
+                Console.WriteLine(soma);
+                break;
+                case "-" :
+                Console.WriteLine(subtracao);
+                break;
+                case "x" :
+                Console.WriteLine(multiplicacao);
+                break;        
+                default:
+                case "/" :
+                Console.WriteLine(multiplicacao);
+                break;
+                Console.WriteLine("Operador inválido");
+                break;
+            }             
+        }
+    }
+}
+    

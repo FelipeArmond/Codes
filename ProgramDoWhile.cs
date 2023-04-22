67b7a12b637019b2aea4a39666beb8806bfde120
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-04-21 22:01:24 -0300

    Add files via upload
    Programa para calcular a área de um triângulo, que não permite a entrada de dados inválidos, ou
    seja, medidas menores ou iguais a 0

---
+++ ProgramDoWhile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int basetr = 1, altura = 1, area;
+
+            
+
+            do
+            {
+                Console.WriteLine("Digite a base do triângulo: ");
+                basetr = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite altura do triângulo: ");
+                altura = int.Parse(Console.ReadLine());
+                if (basetr >0 && altura > 0)
+                {
+                    area = (basetr * altura) /2;
+                    Console.WriteLine("A base do triâgulo é: " + area);
+                }
+                else
+                {
+                    Console.WriteLine("Valores inválidos. A base e a altura devem ser maiores que zero.");
+                }
+                
+            } while (true);
+
+
+        }
+    }
+}

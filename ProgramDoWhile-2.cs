dd186e90f8091b3774d2b631adec2e0acddf2bea
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-04-21 23:07:41 -0300

    Add files via upload
    Programa que recebe vários números, finalizando a entrada de dados com a digitação do número
    30.000. Calcule e mostre:
    a) A soma dos números digitados;
    b) A quantidade de números digitados;
    c) A média dos números digitados;
    d) O maior número digitado;
    e) O menor número digitado;
    f) A porcentagem dos números ímpares entre todos os números digitados

---
+++ ProgramDoWhile-2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int soma = 0, contador = 1, media, maiornum = 0, menornum = int.MaxValue, impar = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+                soma += numero;
+                media = soma / contador;
+
+                if (numero > maiornum)
+                {
+                    maiornum = numero;
+                }
+                if (numero < menornum)
+                {
+                    menornum = numero;
+                }
+                if (numero % 2 == 1)
+                {
+                    impar++;
+                }
+
+                contador++;
+
+            } while (numero != 30000);
+
+            Console.WriteLine("A soma dos números digitados é: " + soma);
+            Console.WriteLine("A quantidade dos números digitados é: " + (contador - 1));
+            Console.WriteLine("A média dos números digitados é : " + media);
+            Console.WriteLine("O maior número digitado é: " + maiornum);
+            Console.WriteLine("O menor número digitado é: " + menornum);
+            Console.WriteLine("A porcentagem dos números ímpares entre todos os números digitados é de " + (impar * 100.0 / (contador - 1)) + "%");
+
+        }
+    }
+}

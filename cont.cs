c20921aaf668f16c778b4bdbe54390b0c3192494
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-04-23 15:25:05 -0300

    Add files via upload
    Programa que recebe dez números inteiros, calcule e mostre a soma dos números pares e a soma dos números ímpares.

---
+++ cont.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, cont = 0, somapar = 0, somaimpar = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número inteiro: ");
+                numero = int.Parse(Console.ReadLine());
+
+
+                if (numero % 2 == 0)
+                {
+                    somapar += numero;
+                }
+                else
+                {
+                    somaimpar += numero;
+                }
+                cont++;
+
+            } while (cont < 10);
+
+            Console.WriteLine("A soma dos números pares é: " + somapar);
+            Console.WriteLine("A soma dos números ímpares é: " + somaimpar);
+        }
+    }
+}

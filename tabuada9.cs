ee2da30de510de50ea18b39d4eb232c0e4313347
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-04-23 16:54:40 -0300

    Add files via upload
    Programa que imprime a tabuada de multiplicação do número 9.

---
+++ tabuada9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Proram
+{
+    class Proram
+    {
+        static void Main(string[] args)
+        {
+           int numero = 9, multiplicado = 0;
+
+           do
+           {
+                Console.WriteLine(" 9 x " + multiplicado  + "= " + numero * multiplicado);
+                multiplicado++;
+
+           } while (multiplicado <= 10);
+
+        }
+    }
+}

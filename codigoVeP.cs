994d4c3fdc9fde4102ebac01c2ea36d9c2361d2d
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-04-28 14:35:30 -0300

    Add files via upload
    Uma loja utiliza o código V para transação à vista e P para transação a prazo. Programa que recebe o código e o valor de 30 transações e dê 10% de desconto nas
    compras à vista. Calcule e mostre:
    a) o valor total das compras à vista; b) o valor total das compras a prazo.

---
+++ codigoVeP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String codigo;
+        double valor = 0, V = 0, P = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Digite o código para transação (V/P): ");
+            codigo = (Console.ReadLine());
+            Console.WriteLine("Qual o valor da transação? ");
+            valor = double.Parse(Console.ReadLine());
+
+            if (codigo == "V" || codigo == "v")
+            {
+                V += valor;
+            }
+            if (codigo == "p" || codigo == "P")
+            {
+                P += valor;
+            }
+
+        }
+        Console.WriteLine("O valor total das compras a vista foi de : R$" + (V - (V * 0.1)));
+        Console.WriteLine("O valor total de compras a prazo foi de: R$" + P);
+    }
+}

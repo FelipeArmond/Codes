bb64d357b15082d605365f347ba5de9c0a686178
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-09-10 14:05:00 -0300

    Add files via upload
    O exercício envolve calcular o fatorial de um número, até o usuário desejar encerrar o programa digitando -1.

---
+++ CalculoFatorial-recursividade.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade1
+{
+    internal class Program        //O exercício envolve calcular o fatorial de um número.
+    {
+        public static int Fatorial (int n)                     
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Fatorial(n - 1);
+            
+
+        }
+        static void Main(string[] args)             
+        {
+            while (true)
+            {
+                
+            Console.WriteLine("Digite o número que você deseja calcular o fatorial (digite -1 para encerrar o programa)");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero == 0 || numero < -1 )
+            {
+                Console.WriteLine("Favor, insira um valor positivo");
+            }
+            else if (numero == -1)
+            {
+                Console.WriteLine("Programa encerrado");
+                break;                   
+            }
+            else
+            {
+                Console.WriteLine(Fatorial(numero));
+            }
+            }
+        }
+    }
+}

f7f3ef72b7a06091ff6eced9bae3a24c102d1f38
FelipeArmond <122803367+FelipeArmond@users.noreply.github.com>
2023-10-02 22:44:38 -0300

    Add files via upload
    Jogo em que o programa escolhe um número aleatório e o jogador tenta adivinhar o número.

---
+++ Adivinha_numero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numeroAleatorio = random.Next(1, 11);
+
+            Console.WriteLine("Tente adivinhar o número gerado aleatoriamente");
+
+            Console.WriteLine(numeroAleatorio);
+          
+            int numero = 0;
+
+            while (numeroAleatorio != numero)
+            {              
+               
+                numero = int.Parse(Console.ReadLine());
+                
+                if (numero == numeroAleatorio)
+                {
+                    Console.WriteLine("Parabéns, você acertou!");
+                }
+                else if (numero < numeroAleatorio)
+                {
+                    Console.WriteLine("Tente um número maior");
+                }
+                else
+                {
+                    Console.WriteLine("Tente um número menor");
+                }
+            }
+        }
+    }
+}
